67afb8023d63276b59397c1b0398935ff2062c56
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-11-30 13:29:32 +0100

    ics bootmenu reboot rewrapper (toolbox replacement)
    update readme, there is no more libreboot in ICS
    
    Change-Id: I8dc5e6cfa5c293c1b4dc60f0d7afbc46a5353daa

--- libreboot/reboot.c
+++ libreboot/reboot.c
@@ -1,15 +1,10 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/reboot.h>
+#include <unistd.h>
 
-//to check:
 //#include "cutils/android_reboot.h"
-//android_reboot(int cmd, int flags, char *arg);
-
-#ifdef RECOVERY_SHELL
-#include "../../../bootable/recovery/libcrecovery/common.h"
-#define system __system
-#endif
 
 #ifndef REBOOT_REASON_DEFAULT
 #define REBOOT_REASON_DEFAULT NULL
@@ -93,7 +88,61 @@ int reboot_wrapper(const char* reason) {
 	if (reboot_with_reason)
 		ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, (char *)reason);
 	else
+		//android_reboot(ANDROID_RB_RESTART, 0, 0);
 		ret = reboot(RB_AUTOBOOT);
 
 	return ret;
 }
+
+//toolbox applet entry (ics)
+int reboot_main(int argc, char *argv[])
+{
+    int ret = 0;
+    int nosync = 0;
+    int poweroff = 0;
+
+    opterr = 0;
+    do {
+        int c;
+
+        c = getopt(argc, argv, "np");
+
+        if (c == EOF) {
+            break;
+        }
+
+        switch (c) {
+        case 'n':
+            nosync = 1;
+            break;
+        case 'p':
+            poweroff = 1;
+            break;
+        case '?':
+            fprintf(stderr, "usage: %s [-n] [-p] [rebootcommand]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    } while (1);
+
+    if(argc > optind + 1) {
+        fprintf(stderr, "%s: too many arguments\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if(!nosync)
+        sync();
+
+    if(poweroff)
+        ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_POWER_OFF, NULL);
+    else if(argc > optind) {
+        ret = reboot_wrapper(argv[optind]);
+    } else
+        ret = reboot_wrapper(NULL);
+    if(ret < 0) {
+        perror("reboot");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "reboot returned\n");
+    return 0;
+}
+
